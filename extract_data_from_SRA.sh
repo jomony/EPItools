f15bcd43cb244ca65b69fa5c770898137a1aee93
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2019-02-11 18:44:27 +0100

    Update extract_data_from_SRA.sh

--- extract_data_from_SRA.sh
+++ extract_data_from_SRA.sh
@@ -1,5 +1,30 @@
 PATH=$PATH:/mnt/INET2/apps/sratoolkit.2.9.2-ubuntu64/bin/
 
+# Glycine max
+ID=SRR5079790
+fastq-dump --split-3 $ID
+ID=SRR6796748
+fastq-dump --split-3 $ID
+
+# Oryza sativa
+ID=SRX4169528
+fastq-dump --split-3 $ID
+ID=DRR029416
+fastq-dump --split-3 $ID
+
 # Arabidopsis lyrata
-fastq-dump --split-3 SRR3880297 
+ID=SRR3880297
+fastq-dump --split-3 $ID
+
+# Arabidopsis thaliana
+ID=SRR4295494
+fastq-dump --split-3 $ID
+ID=SRR4295471
+fastq-dump --split-3 $ID
+
+# Triticum aestivum
+ID=ERR1141918
+fastq-dump --split-3 $ID
+
+
 
