6c700d0e37354eac99efd3d919d8a0289ae2e28b
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2019-02-14 09:25:46 +0100

    Update extract_data_from_SRA.sh

--- extract_data_from_SRA.sh
+++ extract_data_from_SRA.sh
@@ -7,7 +7,7 @@ OUTDIR=$(pwd)
 #
 extract_from_ncbi()
 {
-	for ID in SRR5079790 SRR6796748 SRX4169528 DRR029416 SRR3880297 SRR4295494 SRR4295471 ERR1141918
+	for ID in SRR5079790 SRR6796748 SRR7265433 SRR3880297 SRR4295494 SRR4295471
 	do
 		echo $ID
 		ID_SHORT=$(echo $ID | cut -c1-6)
@@ -35,43 +35,32 @@ extract_fastq_from_sra()
 	# Glycine max
 	ID=SRR5079790.sra
 	fastq-dump --split-3 $ID -O $OUTDIR
-	rm /home/thomas/ncbi/public/sra/*
 
 	ID=SRR6796748.sra
 	fastq-dump --split-3 $ID -O $OUTDIR
-	rm /home/thomas/ncbi/public/sra/*
 
 	# --------------------------------------------------------
 	# Oryza sativa
-	ID=SRX4169528.sra
+	ID=SRR7265433.sra
 	fastq-dump --split-3 $ID -O $OUTDIR
-	rm /home/thomas/ncbi/public/sra/*
 
 	ID=DRR029416.sra
 	fastq-dump --split-3 $ID -O $OUTDIR
-	rm /home/thomas/ncbi/public/sra/*
-
 	# --------------------------------------------------------
 	# Arabidopsis lyrata
 	ID=SRR3880297.sra
 	fastq-dump --split-3 $ID -O $OUTDIR
-	rm /home/thomas/ncbi/public/sra/*
-
 	# --------------------------------------------------------
 	# Arabidopsis thaliana
 	ID=SRR4295494.sra
 	fastq-dump --split-3 $ID -O $OUTDIR
-	rm /home/thomas/ncbi/public/sra/*
 
 	ID=SRR4295471.sra
 	fastq-dump --split-3 $ID -O $OUTDIR
-	rm /home/thomas/ncbi/public/sra/*
-
 	# --------------------------------------------------------
 	# Triticum aestivum
 	ID=ERR1141918.sra
 	fastq-dump --split-3 $ID -O $OUTDIR
-	rm /home/thomas/ncbi/public/sra/*
 	# --------------------------------------------------------
 }
 extract_fastq_from_sra
