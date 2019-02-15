435e17b1a164998474a1f69f633bd1f9fa13ce37
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2019-02-15 13:35:11 +0100

    Create figure3_plot.R

---
+++ figure3_plot.R
@@ -0,0 +1,8 @@
+
+data=read.csv("figure3_memory_plot_input.txt",sep="	",header=F)
+print(data)
+svg("figure3_memory_plot_input.svg")
+par(oma=c(10,0,0,0))
+data=data[order(data[,1]),]
+barplot(data[,2],names=data[,1],las=2,ylab="kbytes")
+dev.off()
