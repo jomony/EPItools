f0b409f983cc5cc7f64dc5cb6843587d8800e568
nthomasCUBE <thomas.nussbaumer@univie.ac.at>
2019-02-15 13:34:13 +0100

    Update monocot_dicots_benchmark_test.sh

--- monocot_dicots_benchmark_test.sh
+++ monocot_dicots_benchmark_test.sh
@@ -2,15 +2,15 @@
 
 # -----------------------------------------------------------
 
-OPT=ALL
+OPT=SUB
 
 # -----------------------------------------------------------
 
 function bismark {
 	for GID in athal gmax alyr osativa wheat1A
 	do
-		./src/run_bismark.sh $GID 0 $OPT
-		./src/run_bismark.sh $GID 1 $OPT
+		#./src/run_bismark.sh $GID 0 $OPT
+		#./src/run_bismark.sh $GID 1 $OPT
 		for STEP in {2..2}
 		do
 			/usr/bin/time -v ./src/run_bismark.sh $GID $STEP $OPT 2> BISMARK_${GID}_${OPT}_time.txt
@@ -21,14 +21,20 @@ function bismark {
 bismark
 
 
-exit
-
 # -----------------------------------------------------------
 
 function bsmap {
-	./src/run_bsmap.sh $GID $OPT
+	for GID in athal gmax alyr osativa wheat1A
+	do
+		echo "::",$GID
+		/usr/bin/time -v ./src/run_bsmap.sh $GID $OPT 2> BSMAP_${GID}_${OPT}_time.txt
+	done
 }
-#bsmap
+bsmap
+
+exit
+
+
 
 # -----------------------------------------------------------
 
